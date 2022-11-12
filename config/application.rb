f85db32b13ef2b242c12c3870c4fe8dabdb78f87
Kouichirou Kimura <814.kimura86@gmail.com>
2022-11-12 23:49:49 +0000

    setting for deploying render.com

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,7 @@ module CodespacesTryRails
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << 'xxx.onrender.com'
   end
 end
